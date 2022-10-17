e83f7c1069f37ca1efd623493e388b7558e72642
GalloTarallo <tommasotodescato@gmail.com>
2022-10-17 17:18:35 +0200

    test

--- new.c
+++ new.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+//test
 int dim_x;
 int dim_y;
 int operation;
 
 void moveright(int deltaX)
 {
-    printf("\033[%dC", deltaX);
+    printf("\033[%dC",deltaX);
 }
-
 void moveup(int deltaY)
 {
-    printf("\033[%dA", deltaY);
+    printf("\033[%dA",deltaY);
 }
 
 int countdigits(int num)
 {
     int count = 0;
-    while(num > 0)
+    while(num>0)
     {
         num = num/10;
         count++;
@@ -27,74 +26,76 @@ int countdigits(int num)
     return count;
 }
 
-int** matrix_make(int x, int y)
+
+int ** matrix_make(int x, int y)
 {
-	int** big_array = malloc(x*y * sizeof(int*));
+	int ** big_array = malloc(x*y*sizeof(int*));
     int counter = 0;
-    for(int i=0; i<y; ++i)
+    printf("\n");
+    for(int i=0;i<y;++i)
 	{
-		int* mini_array = malloc(x * sizeof(int));
+		int * mini_array = malloc(x*sizeof(int));
 		printf("| ");
-        for(int j=0; j<x; ++j)
+
+        for(int j=0;j<x;++j)
 		{
-			scanf("%d", &mini_array[j]);
+			scanf("%d",&mini_array[j]);
             counter = counter + countdigits(mini_array[j]) - 1;
             moveup(1);
-            moveright(j*2+4 + counter);
+            moveright(j*2+4+counter);
 		}
         counter = 0;
 		big_array[i] = mini_array;
+        
         printf("|\n");
     }
 
 	return big_array;
 }
 
-int** matrix_operation(int** m1, int** m2, int x, int y, int operation)
+int ** matrix_operation(int**m1,int**m2,int x,int y,int operation)
 {
-	int** final_matrix = malloc(x*y * sizeof(int*));
-    for(int i=0; i<y; ++i)
-	{	
-        int* mini_array = malloc(x * sizeof(int));
-        for(int j=0; j<x; ++j)
+	int ** final_matrix = malloc(x*y*sizeof(int*));
+    printf("\n");
+    for(int i=0;i<y;++i)
+	{
+		printf("|");	
+        int * mini_array = malloc(x*sizeof(int));
+
+        for(int j=0;j<x;++j)
 		{
             switch(operation)
             {
-                case 0:
-                    mini_array[j] = m1[i][j] + m2[i][j];            
-                    break;
-                case 1:
-                    mini_array[j] = m1[i][j] * m2[i][j];             
-                    break;
-                case 2:
-                    mini_array[j] = m1[i][j] - m2[i][j]; 
-                    break;
+			case 0:
+                mini_array[j] = m1[i][j]+m2[i][j];            
+                break;
+
+            case 1:
+                mini_array[j] = m1[i][j]*m2[i][j];             
+                break;
+
+            case 2:
+                mini_array[j] = m1[i][j]-m2[i][j]; 
+                break;
             }
+            printf(" %d",mini_array[j]);
 		}
         final_matrix[i] = mini_array;
+        printf(" |\n");
     } 
 
 	return final_matrix;
 }
 
-void matrix_print(int** m, int x, int y)
+int ** matrix_transpose(int**m,int x,int y)
 {
-    for(int i=0; i<y; ++i)
+    printf("\n");
+    int ** transposed_matrix = malloc(x*y*sizeof(int*));
+    for(int i=0;i<y;++i)
     {
         printf("|");
-        for(int j=0; j<x; ++j)
-        {
-            printf(" %d", m[i][j]);
-        }
-        printf(" |\n");
-    }
-}
-
-int** matrix_transpose(int** m, int x, int y)
-{
-    for(int i=0; i<y; ++i)
-    {
-        for(int j=0; j<x; ++j)
+        int * mini_array = malloc(x*sizeof(int));
+        for(int j=0;j<x;++j)
         {
             if(j>=y)
             {
@@ -118,23 +119,24 @@ int** matrix_transpose(int** m, int x, int y)
 
 int main()
 {
-    printf("Operation\n 0 - Addiction\n 1 - Product\n 2 - Substraction\n");
-    scanf("%d", &operation);
+    printf("Operation\n 0 - Addiction\n 1 - Product\n 2 - Substraction\n 3 - Transpose");
+    scanf("%d",&operation);
     printf("X Dimension: ");
-    scanf("%d", &dim_x);
+    scanf("%d",&dim_x);
     printf("Y Dimension: ");
-    scanf("%d", &dim_y);
+    scanf("%d",&dim_y);
 
 
-    int** matrix_1 = matrix_make(dim_x, dim_y);    
+    int ** matrix_1 = matrix_make(dim_x,dim_y);    
 
     if(operation<3)
     {
-        int** matrix_2 = matrix_make(dim_x, dim_y);
-        int** output_matrix = matrix_operation(matrix_1, matrix_2, dim_x, dim_y, operation);
-        matrix_print(output_matrix, dim_x, dim_y);
-    }else{
-        matrix_transpose(matrix_1, dim_x, dim_y);
+        int ** matrix_2 = matrix_make(dim_x,dim_y);
+        matrix_operation(matrix_1,matrix_2,dim_x,dim_y,operation);
+    }
+    if(operation==3){
+        matrix_transpose(matrix_1,dim_x,dim_y);
+        printf("a");
     }
         
     return 0;
