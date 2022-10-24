325e46c2ac783d92e01d010a4385b03982b660ab
GalloTarallo <tommasotodescato@gmail.com>
2022-10-24 21:25:15 +0200

    starting to implement new things

--- main.c
+++ main.c
@@ -3,12 +3,23 @@
 #include "operations.h"
 #include "iomatrix.h"
 
-int dim_x;
-int dim_y;
-int operation;
+
+//To implement:
+//	input more than 2 matrixes
+//	matrix inversion
+//	matrix multiplication
+//	matrix dot product
+//	matrix vector product
+//	matrix scalar multiplication
+//	https://ee263.stanford.edu/archive/matrix-primer-lect2.pdf
+
 
 int main()
 {
+	int dim_x;
+	int dim_y;
+	int operation;
+	
 	printf("Operation\n 0 - Addiction\n 1 - Product\n 2 - Substraction\n 3 - Transpose\n");
 	scanf("%d", &operation);
 	
@@ -23,11 +34,16 @@ int main()
 	{
 		int** matrix_2 = matrix_make(dim_x, dim_y);
 		int** output_matrix = matrixes_operation(matrix_1, matrix_2, dim_x, dim_y, operation);
-		
 		matrix_print(output_matrix, dim_x, dim_y);
-	}else{
+	}
+	else if(operation==3)
+	{
 		int** prova = matrix_transpose(matrix_1, dim_x, dim_y);
 		matrix_print(prova, dim_y, dim_x);
+	}
+	else if(operation==4)
+	{
+		printf("pass");	//WIP
 	}
 		
 	return 0;
